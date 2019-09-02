f2b1d3662578136ed2dfd7691b13840ebf561fb9
Brandon Lehmann <brandonlehmann@gmail.com>
2019-09-01 22:36:39 -0400

    Report back the block height that the sync is starting from for logging purposes

--- index.js
+++ index.js
@@ -935,8 +935,9 @@ app.post('/getwalletsyncdata', (req, res) => {
     return res.status(400).send()
   }
 
-  database.legacyGetWalletSyncData(startHeight, startTimestamp, blockHashCheckpoints, blockCount, skipCoinbaseTransactions).then((results) => {
+  database.legacyGetWalletSyncData(startHeight, startTimestamp, blockHashCheckpoints, blockCount, skipCoinbaseTransactions).then((results, fromBlockHeight) => {
     req.body.blockHashCheckpoints = blockHashCheckpoints.length
+    req.body.from = fromBlockHeight || 0
 
     if (results.length >= 1) {
       req.body.range = {
